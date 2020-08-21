e675bd5fe0fefe64cbd041c9cbcb73049de20f1b
Bhaskar Venkatraman <bhaskar@bhaskars-mbp.home>
2020-08-21 03:47:53 -0400

    basic api fetch app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-
-function App() {
-  return (
-    <h1>React App</h1>
-  );
+import React  from 'react';
+import axios from 'axios';
+import './App.css';
+class App extends React.Component{
+  state = { advice :''};
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = async () => {
+    const response = await axios.get('https://api.adviceslip.com/advice');
+    const {advice} = response.data.slip;
+    this.setState({advice:advice});
+  }
+  render(){
+    const {advice} = this.state;
+    return (
+      <h1>{advice}</h1>
+    );
+  }
 }
 
 export default App;
